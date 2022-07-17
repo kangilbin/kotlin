b51a6ad08259853e3532a610e3836690db61e6fa
kangilbin <kib3388@naver.com>
2022-07-17 21:35:59 +0900

    파일별 이미지 설정

--- NovelView/app/src/main/java/com/jacob/novelview/FileLoad.kt
+++ NovelView/app/src/main/java/com/jacob/novelview/FileLoad.kt
@@ -3,6 +3,7 @@ package com.jacob.novelview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.util.Log
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.jacob.novelview.DTO.LoadDTO
@@ -10,11 +11,13 @@ import com.jacob.novelview.adapter.LoadAdapter
 import com.jacob.novelview.databinding.ActivityFileLoadBinding
 import kotlinx.android.synthetic.main.activity_file_load.*
 import java.io.File
+import java.io.IOException
+import java.io.InputStream
 
 class FileLoad : AppCompatActivity(), LoadAdapter.ClickListener {
     private lateinit var binding : ActivityFileLoadBinding
     var list:ArrayList<LoadDTO> = ArrayList()
-    var root = File(Environment.getExternalStorageDirectory().absolutePath)
+    val root = File(Environment.getExternalStorageDirectory().absolutePath)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,46 +33,65 @@ class FileLoad : AppCompatActivity(), LoadAdapter.ClickListener {
         buildDisplayData()
     }
 
-    // 최초 파일 그리기
     private fun buildDisplayData(){
         var files = root?.listFiles()
 
         if(files == null){
             list.add(LoadDTO(R.drawable.ic_folder,"...",root?.parent))
-            Toast.makeText(this, "해당 경로가에 파일이 존재하지 않습니다.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "해당 경로에 파일이 존재하지 않습니다.", Toast.LENGTH_SHORT).show()
             return;
         }
         for(childFile in files){
             if(childFile.isDirectory){
                 list.add(LoadDTO(R.drawable.ic_folder,childFile.name,childFile.absolutePath))
             } else {
-                list.add(LoadDTO(R.drawable.ic_text,childFile.name,childFile.absolutePath))
+                list.add(LoadDTO(R.drawable.ic_txt,childFile.name,childFile.absolutePath))
             }
         }
     }
-    
+    // 외부 저장소 -> 내부 저장소로 복사
+    private fun saveFile(inputStream: InputStream) {
+        val path = filesDir.absolutePath
+
+        File(path).outputStream().use { fileOutput ->
+            inputStream.copyTo(fileOutput)
+        }
+    }
+
     // 폴더 클릭 이벤트
     override fun onItemClick(loadDTO: LoadDTO) {
         var filePath = File(loadDTO.path)
         var files = filePath?.listFiles()
 
-        if(filePath.isDirectory){
-            list.clear();
-            if(!filePath.equals(root)){
-                list.add(LoadDTO(R.drawable.ic_folder,"...",filePath.parent))
-            }
-            if(files != null){
-                for(childFile in files){
-                    if(childFile.isDirectory){
-                        list.add(LoadDTO(R.drawable.ic_folder,childFile.name,childFile.absolutePath))
-                    } else {
-                        list.add(LoadDTO(R.drawable.ic_text,childFile.name,childFile.absolutePath))
-                    }
+        try {
+            if(filePath.isDirectory){
+                list.clear();
+                if(!filePath.equals(root)){
+                    list.add(LoadDTO(R.drawable.ic_folder,"...",filePath.parent))
                 }
-            } else {
-                Toast.makeText(this, "파일이 존재하지 않습니다.", Toast.LENGTH_SHORT).show()
+                if(files != null){
+                    for(childFile in files){
+                        if(childFile.isDirectory){
+                            list.add(LoadDTO(R.drawable.ic_folder,childFile.name,childFile.absolutePath))
+                        } else if (childFile.extension == "txt") {
+                            list.add(LoadDTO(R.drawable.ic_txt,childFile.name,childFile.absolutePath))
+                        } else if(childFile.extension == "zip"){
+                            list.add(LoadDTO(R.drawable.ic_zip,childFile.name,childFile.absolutePath))
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "파일이 존재하지 않습니다.", Toast.LENGTH_SHORT).show()
+                }
+                recyclerView.adapter = LoadAdapter(list, this)
+            } else if (filePath.extension == "txt") {
+                saveFile(filePath.inputStream())
+                Log.d("파일","txt")
+            } else if(filePath.extension == "zip") {
+                saveFile(filePath.inputStream())
+                Log.d("파일","zip")
             }
-            recyclerView.adapter = LoadAdapter(list, this)
+        } catch (ex: IOException) {
+            ex.printStackTrace()
         }
     }
 }
